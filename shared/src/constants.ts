69ad90c7f6c060e06934a949757db5ebf55cb13f
Dmitriy Komleff <komleff@gmail.com>
2025-12-30 13:10:43 +0700

    chore: add zone schema constants

--- shared/src/constants.ts
+++ shared/src/constants.ts
@@ -10,3 +10,8 @@ export const FLAG_PUSHING = 1 << 8;
 export const FLAG_STUNNED = 1 << 9;
 export const FLAG_INVISIBLE = 1 << 10;
 export const FLAG_LEVIATHAN = 1 << 11;
+
+export const ZONE_TYPE_NECTAR = 1;
+export const ZONE_TYPE_ICE = 2;
+export const ZONE_TYPE_LAVA = 3;
+export const ZONE_TYPE_TURBO = 4;
