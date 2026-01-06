39e6b5fed995a202a6616096510c50e3160c4a42
Dmitriy Komlev <komleff@gmail.com>
2026-01-07 02:03:10 +0700

    chore: configure vite for local network and hmr fixed

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,11 +4,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5174,
-    allowedHosts: ['*.overmobile.space'],
-    hmr: {
-      host: 'slime-arena.overmobile.space',
-      protocol: 'wss'
-    }
+    hmr: true
   },
   resolve: {
     alias: {
